c5ed2b917c17504696b9e3902b191d85f36a6403
HyperNotAryanX97 <ahirearyan2016@gmail.com>
2021-08-02 07:42:06 +0000

    Add extras for qssi-user 11 RKQ1.201004.002 V12.0.3.0.RJQCNXM release-keys
    Signed-off-by: HyperNotAryanX97 <ahirearyan2016@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## qssi-user 11 RKQ1.201004.002 V12.0.3.0.RJQCNXM release-keys
+- Manufacturer: xiaomi
+- Platform: bengal
+- Codename: lime
+- Brand: Redmi
+- Flavor: qssi-user
+- Release Version: 11
+- Id: RKQ1.201004.002
+- Incremental: V12.0.3.0.RJQCNXM
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: zh-CN
+- Screen Density: 440
+- Fingerprint: Redmi/lime/lime:11/RKQ1.201004.002/V12.0.3.0.RJQCNXM:user/release-keys
+- OTA version: 
+- Branch: qssi-user-11-RKQ1.201004.002-V12.0.3.0.RJQCNXM-release-keys
+- Repo: redmi_lime_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cat recovery.img.* 2>/dev/null >> recovery.img
+rm -f recovery.img.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat cust/app/customized/partner-com.taobao.taobao_24/partner-com.taobao.taobao_24.apk.* 2>/dev/null >> cust/app/customized/partner-com.taobao.taobao_24/partner-com.taobao.taobao_24.apk
+rm -f cust/app/customized/partner-com.taobao.taobao_24/partner-com.taobao.taobao_24.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.ss.android.ugc.aweme_15/partner-com.ss.android.ugc.aweme_15.apk.* 2>/dev/null >> cust/app/customized/partner-com.ss.android.ugc.aweme_15/partner-com.ss.android.ugc.aweme_15.apk
+rm -f cust/app/customized/partner-com.ss.android.ugc.aweme_15/partner-com.ss.android.ugc.aweme_15.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.eg.android.AlipayGphone_23/partner-com.eg.android.AlipayGphone_23.apk.* 2>/dev/null >> cust/app/customized/partner-com.eg.android.AlipayGphone_23/partner-com.eg.android.AlipayGphone_23.apk
+rm -f cust/app/customized/partner-com.eg.android.AlipayGphone_23/partner-com.eg.android.AlipayGphone_23.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.baidu.searchbox_9/partner-com.baidu.searchbox_9.apk.* 2>/dev/null >> cust/app/customized/partner-com.baidu.searchbox_9/partner-com.baidu.searchbox_9.apk
+rm -f cust/app/customized/partner-com.baidu.searchbox_9/partner-com.baidu.searchbox_9.apk.* 2>/dev/null
+cat vendor/data-app/SmartHome/SmartHome.apk.* 2>/dev/null >> vendor/data-app/SmartHome/SmartHome.apk
+rm -f vendor/data-app/SmartHome/SmartHome.apk.* 2>/dev/null
+cat vendor/lib64/camera/components/com.qti.node.mialgocontrol.so.* 2>/dev/null >> vendor/lib64/camera/components/com.qti.node.mialgocontrol.so
+rm -f vendor/lib64/camera/components/com.qti.node.mialgocontrol.so.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat bootimg/15_dtbdump_~;_4.dtb.* 2>/dev/null >> bootimg/15_dtbdump_~;_4.dtb
+rm -f bootimg/15_dtbdump_~;_4.dtb.* 2>/dev/null
+cat system/system/apex/com.android.vndk.current.apex.* 2>/dev/null >> system/system/apex/com.android.vndk.current.apex
+rm -f system/system/apex/com.android.vndk.current.apex.* 2>/dev/null
+cat system/system/apex/com.android.art.release.apex.* 2>/dev/null >> system/system/apex/com.android.art.release.apex
+rm -f system/system/apex/com.android.art.release.apex.* 2>/dev/null
+cat system/system/data-app/MiShop/MiShop.apk.* 2>/dev/null >> system/system/data-app/MiShop/MiShop.apk
+rm -f system/system/data-app/MiShop/MiShop.apk.* 2>/dev/null
+cat system/system/data-app/Youpin/Youpin.apk.* 2>/dev/null >> system/system/data-app/Youpin/Youpin.apk
+rm -f system/system/data-app/Youpin/Youpin.apk.* 2>/dev/null
+cat system/system/priv-app/Browser/Browser.apk.* 2>/dev/null >> system/system/priv-app/Browser/Browser.apk
+rm -f system/system/priv-app/Browser/Browser.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiVideo/MiuiVideo.apk.* 2>/dev/null >> system/system/priv-app/MiuiVideo/MiuiVideo.apk
+rm -f system/system/priv-app/MiuiVideo/MiuiVideo.apk.* 2>/dev/null
