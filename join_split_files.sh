ccb7fde8a34ddb18e075ae4e1b8ebcd3af87cf28
HyperNotAryanX97 <ahirearyan2016@gmail.com>
2021-12-15 08:39:33 +0000

    Add extras for qssi-user 11 RKQ1.201004.002 V12.5.5.0.RJQCNXM release-keys
    Signed-off-by: HyperNotAryanX97 <ahirearyan2016@gmail.com>

---
+++ join_split_files.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+cat bootimg/15_dtbdump_FEuDa\.dtb.* 2>/dev/null >> bootimg/15_dtbdump_FEuDa\.dtb
+rm -f bootimg/15_dtbdump_FEuDa\.dtb.* 2>/dev/null
+cat recovery.img.* 2>/dev/null >> recovery.img
+rm -f recovery.img.* 2>/dev/null
+cat system/system/priv-app/Browser/Browser.apk.* 2>/dev/null >> system/system/priv-app/Browser/Browser.apk
+rm -f system/system/priv-app/Browser/Browser.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiVideo/MiuiVideo.apk.* 2>/dev/null >> system/system/priv-app/MiuiVideo/MiuiVideo.apk
+rm -f system/system/priv-app/MiuiVideo/MiuiVideo.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/apex/com.android.vndk.current.apex.* 2>/dev/null >> system/system/apex/com.android.vndk.current.apex
+rm -f system/system/apex/com.android.vndk.current.apex.* 2>/dev/null
+cat system/system/apex/com.android.art.release.apex.* 2>/dev/null >> system/system/apex/com.android.art.release.apex
+rm -f system/system/apex/com.android.art.release.apex.* 2>/dev/null
+cat system/system/app/Music/Music.apk.* 2>/dev/null >> system/system/app/Music/Music.apk
+rm -f system/system/app/Music/Music.apk.* 2>/dev/null
+cat system/system/app/VoiceAssist/VoiceAssist.apk.* 2>/dev/null >> system/system/app/VoiceAssist/VoiceAssist.apk
+rm -f system/system/app/VoiceAssist/VoiceAssist.apk.* 2>/dev/null
+cat system/system/data-app/MiFinance/MiFinance.apk.* 2>/dev/null >> system/system/data-app/MiFinance/MiFinance.apk
+rm -f system/system/data-app/MiFinance/MiFinance.apk.* 2>/dev/null
+cat system/system/data-app/MiShop/MiShop.apk.* 2>/dev/null >> system/system/data-app/MiShop/MiShop.apk
+rm -f system/system/data-app/MiShop/MiShop.apk.* 2>/dev/null
+cat system/system/data-app/Youpin/Youpin.apk.* 2>/dev/null >> system/system/data-app/Youpin/Youpin.apk
+rm -f system/system/data-app/Youpin/Youpin.apk.* 2>/dev/null
+cat system/system/data-app/VipAccount/VipAccount.apk.* 2>/dev/null >> system/system/data-app/VipAccount/VipAccount.apk
+rm -f system/system/data-app/VipAccount/VipAccount.apk.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system_ext/priv-app/Settings/Settings.apk
+rm -f system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat vendor/lib64/camera/components/com.qti.node.mialgocontrol.so.* 2>/dev/null >> vendor/lib64/camera/components/com.qti.node.mialgocontrol.so
+rm -f vendor/lib64/camera/components/com.qti.node.mialgocontrol.so.* 2>/dev/null
+cat vendor/data-app/SmartHome/SmartHome.apk.* 2>/dev/null >> vendor/data-app/SmartHome/SmartHome.apk
+rm -f vendor/data-app/SmartHome/SmartHome.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.taobao.taobao_24/partner-com.taobao.taobao_24.apk.* 2>/dev/null >> cust/app/customized/partner-com.taobao.taobao_24/partner-com.taobao.taobao_24.apk
+rm -f cust/app/customized/partner-com.taobao.taobao_24/partner-com.taobao.taobao_24.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.baidu.searchbox_9/partner-com.baidu.searchbox_9.apk.* 2>/dev/null >> cust/app/customized/partner-com.baidu.searchbox_9/partner-com.baidu.searchbox_9.apk
+rm -f cust/app/customized/partner-com.baidu.searchbox_9/partner-com.baidu.searchbox_9.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.UCMobile_11/partner-com.UCMobile_11.apk.* 2>/dev/null >> cust/app/customized/partner-com.UCMobile_11/partner-com.UCMobile_11.apk
+rm -f cust/app/customized/partner-com.UCMobile_11/partner-com.UCMobile_11.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.ss.android.ugc.aweme_15/partner-com.ss.android.ugc.aweme_15.apk.* 2>/dev/null >> cust/app/customized/partner-com.ss.android.ugc.aweme_15/partner-com.ss.android.ugc.aweme_15.apk
+rm -f cust/app/customized/partner-com.ss.android.ugc.aweme_15/partner-com.ss.android.ugc.aweme_15.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.eg.android.AlipayGphone_23/partner-com.eg.android.AlipayGphone_23.apk.* 2>/dev/null >> cust/app/customized/partner-com.eg.android.AlipayGphone_23/partner-com.eg.android.AlipayGphone_23.apk
+rm -f cust/app/customized/partner-com.eg.android.AlipayGphone_23/partner-com.eg.android.AlipayGphone_23.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> product/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
