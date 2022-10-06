9a0d90a2aacf54892211b95386ce0684cf020745
HyperNotAryanX97 <ahirearyan2016@gmail.com>
2022-10-06 03:25:21 +0000

    Add extras for qssi-user 12 SKQ1.211202.001 V13.1.22.9.28.DEV release-keys
    Signed-off-by: HyperNotAryanX97 <ahirearyan2016@gmail.com>

---
+++ join_split_files.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/priv-app/AndroidS-Extraphoto/AndroidS-Extraphoto.apk.* 2>/dev/null >> system/system/priv-app/AndroidS-Extraphoto/AndroidS-Extraphoto.apk
+rm -f system/system/priv-app/AndroidS-Extraphoto/AndroidS-Extraphoto.apk.* 2>/dev/null
+cat system/system/priv-app/MIUIBrowser/MIUIBrowser.apk.* 2>/dev/null >> system/system/priv-app/MIUIBrowser/MIUIBrowser.apk
+rm -f system/system/priv-app/MIUIBrowser/MIUIBrowser.apk.* 2>/dev/null
+cat system/system/priv-app/MIUIMusic/MIUIMusic.apk.* 2>/dev/null >> system/system/priv-app/MIUIMusic/MIUIMusic.apk
+rm -f system/system/priv-app/MIUIMusic/MIUIMusic.apk.* 2>/dev/null
+cat system/system/priv-app/MIUIVideo/MIUIVideo.apk.* 2>/dev/null >> system/system/priv-app/MIUIVideo/MIUIVideo.apk
+rm -f system/system/priv-app/MIUIVideo/MIUIVideo.apk.* 2>/dev/null
+cat system/system/priv-app/MIUIGallery/MIUIGallery.apk.* 2>/dev/null >> system/system/priv-app/MIUIGallery/MIUIGallery.apk
+rm -f system/system/priv-app/MIUIGallery/MIUIGallery.apk.* 2>/dev/null
+cat system/system/priv-app/MIUISecurityCenter/MIUISecurityCenter.apk.* 2>/dev/null >> system/system/priv-app/MIUISecurityCenter/MIUISecurityCenter.apk
+rm -f system/system/priv-app/MIUISecurityCenter/MIUISecurityCenter.apk.* 2>/dev/null
+cat system/system/data-app/SmartHome/SmartHome.apk.* 2>/dev/null >> system/system/data-app/SmartHome/SmartHome.apk
+rm -f system/system/data-app/SmartHome/SmartHome.apk.* 2>/dev/null
+cat system/system/data-app/MIUINotes/MIUINotes.apk.* 2>/dev/null >> system/system/data-app/MIUINotes/MIUINotes.apk
+rm -f system/system/data-app/MIUINotes/MIUINotes.apk.* 2>/dev/null
+cat system/system/data-app/MIUIYoupin/MIUIYoupin.apk.* 2>/dev/null >> system/system/data-app/MIUIYoupin/MIUIYoupin.apk
+rm -f system/system/data-app/MIUIYoupin/MIUIYoupin.apk.* 2>/dev/null
+cat system/system/data-app/MIUIScanner/MIUIScanner.apk.* 2>/dev/null >> system/system/data-app/MIUIScanner/MIUIScanner.apk
+rm -f system/system/data-app/MIUIScanner/MIUIScanner.apk.* 2>/dev/null
+cat system/system/data-app/MiShop/MiShop.apk.* 2>/dev/null >> system/system/data-app/MiShop/MiShop.apk
+rm -f system/system/data-app/MiShop/MiShop.apk.* 2>/dev/null
+cat system/system/data-app/MIUIDuokanReader/MIUIDuokanReader.apk.* 2>/dev/null >> system/system/data-app/MIUIDuokanReader/MIUIDuokanReader.apk
+rm -f system/system/data-app/MIUIDuokanReader/MIUIDuokanReader.apk.* 2>/dev/null
+cat system/system/data-app/Health/Health.apk.* 2>/dev/null >> system/system/data-app/Health/Health.apk
+rm -f system/system/data-app/Health/Health.apk.* 2>/dev/null
+cat system/system/data-app/MIMediaEditor/MIMediaEditor.apk.* 2>/dev/null >> system/system/data-app/MIMediaEditor/MIMediaEditor.apk
+rm -f system/system/data-app/MIMediaEditor/MIMediaEditor.apk.* 2>/dev/null
+cat system/system/app/SogouInput/SogouInput.apk.* 2>/dev/null >> system/system/app/SogouInput/SogouInput.apk
+rm -f system/system/app/SogouInput/SogouInput.apk.* 2>/dev/null
+cat system/system/app/VoiceAssist/VoiceAssist.apk.* 2>/dev/null >> system/system/app/VoiceAssist/VoiceAssist.apk
+rm -f system/system/app/VoiceAssist/VoiceAssist.apk.* 2>/dev/null
+cat bootimg/15_dtbdump_3s-_t.dtb.* 2>/dev/null >> bootimg/15_dtbdump_3s-_t.dtb
+rm -f bootimg/15_dtbdump_3s-_t.dtb.* 2>/dev/null
+cat product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> product/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat vendor/lib64/camera/components/com.qti.node.mialgocontrol.so.* 2>/dev/null >> vendor/lib64/camera/components/com.qti.node.mialgocontrol.so
+rm -f vendor/lib64/camera/components/com.qti.node.mialgocontrol.so.* 2>/dev/null
+cat system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null >> system_ext/apex/com.android.vndk.v30.apex
+rm -f system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null
+cat system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system_ext/priv-app/Settings/Settings.apk
+rm -f system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
